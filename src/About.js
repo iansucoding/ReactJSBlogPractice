03aa89e9abd2f142ee38b30b738f0e9a5588fea8
ian <iamsu@mirle.com.tw>
2022-01-26 17:24:03 +0800

    init

---
+++ src/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+      <main className="About">
+        <h2>About</h2>
+        <p>Learn React Router</p>
+      </main>
+    );
+  }
+  
+  export default About;
